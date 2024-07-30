5d85f5d8a924df33e3a38142fce8429819b15471
andapar <160366488+andapar@users.noreply.github.com>
2024-07-30 20:11:17 +0300

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,18 @@
+
+"use client";
+
+import React from "react";
+import styles from "./page.module.css";
+import Chat from "../../components/chat";
+
+const Home = () => {
+  return (
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <Chat />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
